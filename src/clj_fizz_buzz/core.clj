38f23a974b1fda6bb4864293f6eb1b30fcab014e
Henk Reder <henk@yetanalytics.com>
2017-07-31 23:18:21 -0400

    wrote code for fizz-buzz challenge

--- src/clj_fizz_buzz/core.clj
+++ src/clj_fizz_buzz/core.clj
@@ -1,6 +1,28 @@
 (ns clj-fizz-buzz.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn fizz-buzz [t]
+  (loop [iterator t
+         result []]
+    (if (= iterator 0)
+      result
+      (let [mod-a (mod iterator 3)
+            mod-b (mod iterator 5)]
+      (cond 
+        (and (= mod-a 0) (= mod-b 0))
+        (recur (dec iterator) (conj result "fizzbuzz"))
+        (= mod-a 0)
+        (recur (dec iterator) (conj result "fizz"))
+        (= mod-b 0)
+        (recur (dec iterator) (conj result "buzz"))
+        :else
+        (recur (dec iterator) (conj result iterator)))))))
+
+(fizz-buzz 15)
+
+(loop [iterator 100]
+  (if (= iterator 0)
+    0
+    (recur (dec iterator))))
+
+(def foo 15)
+
--- test/clj_fizz_buzz/core_test.clj
+++ test/clj_fizz_buzz/core_test.clj
@@ -2,6 +2,5 @@
   (:require [clojure.test :refer :all]
             [clj-fizz-buzz.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest fizz-buzz-test
+  (is (= (fizz-buzz 15) ["fizzbuzz" 14 13 "fizz" 11 "buzz" "fizz" 8 7 "fizz" "buzz" 4 "fizz" 2 1])))
