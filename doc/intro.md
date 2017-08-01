3c98429dd64cfd2f7730d5913be41df7c45e8d65
Henk Reder <henk@yetanalytics.com>
2017-07-31 22:17:48 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-fizz-buzz
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_fizz_buzz/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-fizz-buzz.core-test
+  (:require [clojure.test :refer :all]
+            [clj-fizz-buzz.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
